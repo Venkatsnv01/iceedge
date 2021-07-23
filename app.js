613be12028f2ddf84bd0e43ea0c2815971212661
Venkatsnv01 <venkatsnv01@gmail.com>
2021-07-23 10:33:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+function mainFunction() {
+  var user = document.getElementById("username");
+  var pass = document.getElementById("password");
+
+  user.onfocus = function () {
+    if (user.value === "Username") {
+      user.value = "";
+    }
+  };
+  user.onblur = function () {
+    if (user.value === "") {
+      user.value = "Username";
+    }
+  };
+
+  pass.onfocus = function () {
+    if (pass.value === "Password") {
+      pass.value = "";
+    }
+  };
+  pass.onblur = function () {
+    if (pass.value === "") {
+      pass.value = "Password";
+    }
+  };
+}
+
+window.onload = function () {
+  mainFunction();
+};
